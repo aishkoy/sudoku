0d28c1639ac42ddd9ac1aeb9a0e85bd7f3cf589d
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2024-10-30 21:04:02 +0600

    added a method to have random numbers on a row

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Random;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main{
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[][] field = new int[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            boolean[] used = new boolean[9];
+            for (int j = 0; j < 9; j++) {
+                int newElement;
+                do {
+                    newElement = rand.nextInt(9);
+                } while (used[newElement]);
+                field[i][j] = newElement + 1;
+                used[newElement] = true;
+                System.out.print(field[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
